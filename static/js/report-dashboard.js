614df398ed73689cf3e7805568919cf8475e7555
SantoshP15 <santoshpatil15620@gmail.com>
2026-08-24 12:52:24 +0530

    refershed tab added

--- static/js/report-dashboard.js
+++ static/js/report-dashboard.js
@@ -251,3 +251,20 @@ document.addEventListener(
 
     }
 );
+function updateLastRefreshed() {
+    const now = new Date();
+
+    const formattedDateTime = now.toLocaleString('en-IN', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+
+    document.getElementById('lastRefreshed').textContent = formattedDateTime;
+}
+
+updateLastRefreshed();
